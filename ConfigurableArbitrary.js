da7126cd84b7dfd655c8f7e5a0758412c48b38ec
Ryan Leonard <rhinoisnot@gmail.com>
2017-07-10 10:52:11 -0400

    Added documentation source, build, and publication.

--- ConfigurableArbitrary.js
+++ ConfigurableArbitrary.js
@@ -1,5 +1,14 @@
+/**
+ * A "factory" for JSVerify generators that can be configured or composed to better meet the needs of individual tests.
+*/
 class ConfigurableArbitrary {
 
+  /**
+   * Determines if the given input is a JSVerify `arbitrary`.
+   * @param {Any} input the value to check.
+   * @return {Boolean} `true` if input looks like a JSVerify `arbitrary`.
+   * @see https://github.com/jsverify/jsverify#types
+  */
   static isArbitrary(input) {
     return (typeof input === "object" || typeof input === "function") && typeof input.generator === "function";
   }
